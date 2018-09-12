d53f1e7723141da14e80a1abc46251e1961d3efe
nlennon <nik.lennon@eleadcrm.com>
2018-09-12 15:24:07 -0500

    change column widths

--- src/components/contact.js
+++ src/components/contact.js
@@ -7,7 +7,7 @@ class Contact extends Component {
         return(
             <div>
                 <Grid className="contact-grid">
-                    <Cell col={6}>
+                    <Cell col={4}>
                         <h2>Nikolas Lennon</h2>
                         <img 
                           src="https://cdn0.iconfinder.com/data/icons/user-pictures/100/male-512.png"
@@ -18,7 +18,7 @@ class Contact extends Component {
                             Please use the following methods of contact to the right to reach out to me. I am mostly available by email but will respond to phone calls and texts on a regular basis. 
                         </p>
                     </Cell>
-                    <Cell col={6}>
+                    <Cell col={8}>
                         <h2>Contact Me</h2>
                         <hr />
                         <div className="contact-list">
@@ -35,14 +35,16 @@ class Contact extends Component {
                             <ListItem>
                                 <ListItemContent style={{ fontSize: '30px', fontFamily: 'Anton'}}>
                                     <i className="fa fa-envelope-square" aria-hidden="true" />
-                                    <h4>lennon.nikolas@gmail.com</h4>                                </ListItemContent>
+                                    <h4>lennon.nikolas@gmail.com</h4>                                
+                                </ListItemContent>
                             </ListItem>
                             </div>
                             <div>
                             <ListItem>
                                 <ListItemContent style={{ fontSize: '30px', fontFamily: 'Anton'}}>
                                     <i className="fa fa-skype" aria-hidden="true" />
-                                    <h4>lennon.nikolas</h4>                                </ListItemContent>
+                                    <h4>lennon.nikolas</h4>                                
+                                </ListItemContent>
                             </ListItem>
                             </div>
                             </List>   
